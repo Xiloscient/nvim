72ef28d76be5b867877325051376aefad369e672
Xiloscient <felix.kunze77@gmail.com>
2025-01-05 17:19:01 +0100

    icon gone

--- init.lua
+++ init.lua
@@ -14,16 +14,33 @@ vim.opt.rtp:prepend(lazypath)
 require("plugins")
 require("setup")
 require("colorscheme_setup")
+vim.cmd("colorscheme gruvbox")
 
 require("cmp_setup")
 require("lsp_setup")
 require("vsnip_setup")
 
 require'nvim-treesitter.configs'.setup { highlight = { enable = true }, indent = { enable = true } }
-require('lualine').setup{ options = { theme = 'gruvbox'} }
+require('lualine').setup{ options = { theme = 'gruvbox', icons_enabled = false} }
 
 require('nvim-autopairs').setup()
-require("nvim-tree").setup()
+
+require("nvim-tree").setup({ -- BEGIN_DEFAULT_OPTS
+      renderer = {
+        icons = {
+          show = {
+            file = false,
+            folder = false,
+            folder_arrow = false,
+            git = false,
+            modified = false,
+            diagnostics = false,
+            bookmarks = false,
+          },
+        },
+      },
+    }
+)
 
 require('gitsigns').setup()
 require("ibl").setup()
--- lua/colorscheme_setup.lua
+++ lua/colorscheme_setup.lua
@@ -22,4 +22,3 @@ require("gruvbox").setup({
   dim_inactive = false,
   transparent_mode = true,
 })
-vim.cmd("colorscheme gruvbox")
