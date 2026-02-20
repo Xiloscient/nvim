67f836da97cc3ab91e356d569ecfd62a130e8ffb
Xiloscient <felix.kunze77@gmail.com>
2026-02-19 22:17:00 -0800

    that's all folks

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
--- Set <Space> as leader
 vim.g.mapleader = " "
-local opts = { noremap = true, silent = true }
+local opts = {noremap = true, silent = true}
 
 -- Delete without yanking
 vim.keymap.set("n", "x", '"_x', opts)
@@ -17,7 +16,6 @@ vim.keymap.set("n", "<C-f>", ":FzfLua files<CR>", opts)
 vim.keymap.set("n", "<C-g>", ":FzfLua grep_project<CR>", opts)
 vim.keymap.set("n", "<leader>/", ":FzfLua lines<CR>", opts)
 vim.keymap.set("n", "<leader>o", ":FzfLua loclist<CR>", opts)
-vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", opts)
 
 -- Insert mode arrow controls
 vim.keymap.set("i", "<C-h>", "<Left>", opts)
@@ -39,7 +37,6 @@ vim.keymap.set('n', '<leader>D', function() vim.diagnostic.setloclist({open = tr
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
-
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
@@ -52,16 +49,13 @@ vim.opt.ignorecase = true
 vim.opt.cursorline = true
 vim.opt.cursorcolumn = true
 vim.opt.scrolloff = 7
-
+vim.opt.signcolumn = "yes"
 vim.opt.visualbell = true
 
 vim.opt.clipboard = "unnamed"
 
 vim.opt.writebackup = false
-
 vim.opt.grepprg = "rg --vimgrep --smart-case --follow"
-
-vim.opt.signcolumn = "yes"
 
 vim.g.rustfmt_autosave = 0
 
@@ -71,9 +65,9 @@ vim.lsp.enable('rust_analyzer')
 vim.lsp.enable('clangd')
 
 vim.diagnostic.config({
-  virtual_text = { severity = { min = vim.diagnostic.severity.ERROR } },
-  signs        = { severity = { min = vim.diagnostic.severity.ERROR } },
-  underline    = { severity = { min = vim.diagnostic.severity.ERROR } },
+  virtual_text = {severity = {min = vim.diagnostic.severity.ERROR}},
+  signs        = {severity = {min = vim.diagnostic.severity.ERROR}},
+  underline    = {severity = {min = vim.diagnostic.severity.ERROR}},
   severity_sort = true,
   update_in_insert = false,
 })
@@ -83,35 +77,32 @@ vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/lazy/lazy.nvim")
 require('lazy').setup({
 
     ------------------ visual stuff ------------------
-    { "ellisonleao/gruvbox.nvim",
+    {"ellisonleao/gruvbox.nvim",
       config = function()
-        require("gruvbox").setup({ contrast = "hard", transparent_mode = true })
+        require("gruvbox").setup({contrast = "hard", transparent_mode = true})
         vim.cmd("colorscheme gruvbox")
-      end },
-    { "nvim-lualine/lualine.nvim",
-      opts = { options = { theme = 'gruvbox', icons_enabled = false } } },
-
+      end},
+    {"nvim-lualine/lualine.nvim", opts = {options = {theme = 'gruvbox', icons_enabled = false}}},
 
     ------------------ editor -----------------
     "ibhagwan/fzf-lua",
 
-    { 'nvim-tree/nvim-tree.lua',
-      opts = { renderer = { icons = {
-        show = { file = false, folder = false, folder_arrow = false, git = false }
-      }}} },
-    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+    {'nvim-tree/nvim-tree.lua',
+      opts = {renderer = {icons = {
+        show = {file = false, folder = false, folder_arrow = false, git = false}
+      }}}},
+    {"lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {}},
 
     ------------------ completion and linting -----------------
     'neovim/nvim-lspconfig',
-    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate',
+    {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate',
       main = 'nvim-treesitter.configs',
-      opts = { highlight = { enable = true }, indent = { enable = true } } },
+      opts = {highlight = {enable = true}, indent = {enable = true}}},
 
-    {
-      'saghen/blink.cmp',
-      dependencies = { 'rafamadriz/friendly-snippets' },
+    {'saghen/blink.cmp',
+      dependencies = {'rafamadriz/friendly-snippets'},
       version = '1.*',
-      opts = { completion = { documentation = { auto_show = true } } },
+      opts = {completion = {documentation = {auto_show = true}}},
     },
 
     'rust-lang/rust.vim',
@@ -120,21 +111,19 @@ require('lazy').setup({
     'tpope/vim-surround',   -- better surrounding chars
     'tpope/vim-commentary', -- comment/uncomment
     'ntpeters/vim-better-whitespace', -- whitespace cleanup
-    { 'windwp/nvim-autopairs', opts = {} },
+    {'windwp/nvim-autopairs', opts = {}},
 
     ------------------ git ------------------
-    "kdheepak/lazygit.nvim",
-    { 'lewis6991/gitsigns.nvim',
+    'tpope/vim-fugitive',
+    {'lewis6991/gitsigns.nvim',
         opts = {
             signs = {
-                add = { text = '+' },
-                change = { text = '~' },
-                delete = { text = '_' },
-                topdelete = { text = '‾' },
-                changedelete = { text = '~' },
+                add = {text = '+'},
+                change = {text = '~'},
+                delete = {text = '_'},
+                topdelete = {text = '‾'},
+                changedelete = {text = '~'},
             }
         }
     },
-
-    'tpope/vim-fugitive'
 })
